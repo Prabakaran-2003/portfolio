b78ce9541f9596a4b6cc535fb655309f1d83de93
Prabakaran-2003 <149931653+Prabakaran-2003@users.noreply.github.com>
2025-05-16 14:56:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Add a little animation or interactivity if needed
 console.log("Portfolio site loaded.");
+window.scrollTo({
+  top: target.offsetTop - 60,
+  behavior: "smooth"
+});
 
 // Example: Add smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
